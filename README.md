4da9e80d617723b4af7fed89fd31e163dacfbcd4
David Marby <david@dmarby.se>
2014-04-26 16:12:50 +0200

    Finish url shortener

---
+++ README.md
@@ -0,0 +1,2 @@
+Webbdesignkurser Urlshortener [![Build Status](https://travis-ci.org/Webbdesignkurser/urlshortener.svg?branch=master)](https://travis-ci.org/Webbdesignkurser/urlshortener)
+=============
---
+++ index.js
@@ -0,0 +1,48 @@
+var restify = require('restify');
+var unirest = require('unirest');
+var urls = {};
+
+function getUrls() {
+  var url = "http://webbdesignkurser.github.io/website/short.json"
+  //var url = "http://localhost:4000/short.json"
+
+  unirest.get(url).end(function (response) {
+      if (response.error) {
+          return;
+      }
+      urls = response.body;
+  });
+};
+
+getUrls();
+
+setInterval(function() {
+    unirest.get('http://wdk.se').end(function (response) {});
+}, 1000 * 60 * 20);
+
+
+var app = restify.createServer();
+
+app.get('/', function (req, res, next) {
+  res.header('Location', 'http://webbdesignkurser.se');
+  res.send(301);
+})
+
+app.get("/:hash", function (req, res, next) {
+    if(urls[req.params.hash] != null) {
+      res.header('Location', urls[req.params.hash]);
+      res.send(301);
+    } else {
+      res.send("Not found!");
+      res.send(404);
+    }
+});
+
+app.post('/webhook', function (req, res, next) {
+    res.send(200);
+    setTimeout(function() {
+      getUrls();
+    }, 1000 * 60 * 3);
+});
+
+app.listen(process.env.PORT || 5000);
